c66ca6ad6fd371bc225337f87715ae1f56e5ae45
Bob Petersen <33petersen@gmail.com>
2018-10-27 16:14:44 -0500

    styled header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import thermal from './thermal.jpg';
 import './App.css';
 import axios from 'axios';
-import Gallery from '../Gallery/Gallery'
+// import Gallery from '../Gallery/Gallery'
 
 
 
@@ -50,20 +50,12 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Low Cost Thermal Camera
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={thermal} className="App-logo" alt="logo" />
         </header>
-        <Gallery list={this.state.imageList}/>
+        {/* <Gallery list={this.state.imageList}/> */}
       </div>
     );
   }
