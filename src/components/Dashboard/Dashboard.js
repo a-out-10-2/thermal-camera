c08482c33868075fb369b85b855ef67e88eeb919
Bob Petersen <33petersen@gmail.com>
2018-10-27 14:32:03 -0500

    made a few changes

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import axios from 'axios';
+import Gallery from './Gallery/Gallery';
+import Header from './Header/Header'
+
+class Dashboard extends Component {
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
