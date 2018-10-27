c9e4c581c05dc2e0cda5462a5b1661bdb282825b
Bob Petersen <33petersen@gmail.com>
2018-10-27 14:59:58 -0500

    changes

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -3,24 +3,23 @@ import React, { Component } from 'react';
 
 
 class Gallery extends Component {
-
   
-    render() {
-        return (
-          <section>
-            <h2>Your Thermal Images</h2>
-            <ul>
-              { this.props.list.map( (image, index) => 
-                <div key={index}>
-                  <img alt='' src={image.path}/>
-                  <p>Date: {image.date}</p>
-                  <p>Time: {image.time}</p>
-                </div>
-                ) }
-            </ul>
-          </section>
-        );
-      }
-    }
+  render() {
+    return (
+      <section>
+        <h2>Your Thermal Images</h2>
+        <ul>
+          { this.props.list.map( (image, index) => 
+            <div key={index}>
+              <img alt='' src={image.path}/>
+              <p>Date: {image.date}</p>
+              <p>Time: {image.time}</p>
+            </div>
+          )}
+        </ul>
+      </section>
+    );
+  }
+}
 
 export default Gallery;
