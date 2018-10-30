78fa4807357547113c6455ac66fb00fcf3ce98b5
Bob Petersen <33petersen@gmail.com>
2018-10-30 11:13:44 -0500

    added nav component with classNames for future styling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,28 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import './nav.css'
 
-const Nav = (props) => (
-    <div className="navbar">
-        <ul>
-            <li>
-                <Link to="/dashboard">
-                    Home Page
-                </Link>
-            </li>
-            <li>
-                <Link to="/gallery">
-                    Gallery
-                </Link>
-            </li>
-            <li>
-                <Link to="/search">
-                    Search
-                </Link>
-            </li>
-        </ul>
+const Nav = () => (
+  <div className="GridHeader">
+    <div id="home">
+      <Link to="/dash">
+        Home Page
+      </Link>
     </div>
+    <div id="gallery">
+      <Link to="/gallery">
+        Gallery
+      </Link>
+    </div>
+    <div id="search">
+      <Link to="/search">
+        Search
+      </Link>
+    </div>
+  </div>
 );
 
-export default connect()(Nav);
+export default Nav;
